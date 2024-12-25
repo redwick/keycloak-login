f595593435be3600d078d1a6d110e85738ecd291
Bogdan Isaev <redzor@live.com>
2024-12-25 18:23:32 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, effect, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Keycloak from 'keycloak-js';
+import {KEYCLOAK_EVENT_SIGNAL, KeycloakEventType, ReadyArgs, typeEventArgs} from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,34 @@ import Keycloak from 'keycloak-js';
 })
 export class AppComponent {
   title = 'kc';
+  user: any;
+  authenticated: boolean = false;
 
   constructor(public kc: Keycloak) {
+    const keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
 
+    effect(() => {
+      const keycloakEvent = keycloakSignal();
+
+      if (keycloakEvent.type === KeycloakEventType.Ready) {
+        this.authenticated = typeEventArgs<ReadyArgs>(keycloakEvent.args);
+        this.load();
+      }
+
+      if (keycloakEvent.type === KeycloakEventType.AuthLogout) {
+        this.authenticated = false;
+      }
+    });
   }
   login() {
     this.kc.login().then(res => {
-
+      console.log(res);
     });
   }
   load() {
     this.kc.loadUserProfile().then(profile => {
-      alert(JSON.stringify(profile));
+      this.user = profile;
+      console.log(this.user);
     });
   }
 
